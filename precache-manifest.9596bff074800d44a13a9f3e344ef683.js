5b2e887a8a4be6417d513072e4fe2e2d9c52dd5d
bogeresiraje <bogeresiraje25@gmail.com>
2020-10-20 12:42:46 +0300

    Updates

---
+++ precache-manifest.9596bff074800d44a13a9f3e344ef683.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "beac0830874cd20211745c4dd96d59b2",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "ec611193ca0cb842ec78",
+    "url": "/portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "194d84014cd9386c38b0",
+    "url": "/portfolio/static/js/2.1ed03ce3.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/portfolio/static/js/2.1ed03ce3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec611193ca0cb842ec78",
+    "url": "/portfolio/static/js/main.917c047c.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "2a0b57bedf5b6b8943925974fcd7900c",
+    "url": "/portfolio/static/media/api.2a0b57be.jpeg"
+  },
+  {
+    "revision": "35a467652747f7863f62ce1350d70c51",
+    "url": "/portfolio/static/media/mobileapp.35a46765.webp"
+  },
+  {
+    "revision": "43e2463199d9acba1f379eaa084daf65",
+    "url": "/portfolio/static/media/web.43e24631.jpg"
+  }
+]);
